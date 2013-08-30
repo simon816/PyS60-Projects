cae48c180091da79bfcf3502e455a18d8ab28b71
simon816 <simonbarnes1@googlemail.com>
2013-08-30 17:59:56 +0100

    Improves SyntaxHighlighter by adding support for my new parser

--- CommonLib/simon816/SyntaxHighlighter.py
+++ CommonLib/simon816/SyntaxHighlighter.py
@@ -1,14 +1,28 @@
 import appuifw
 from simon816.ExtendedEditor import ExtendedEditor
 from HTMLParser import *#HTMLParser
-from simon816.jsparser import JSParser
+from simon816.JSParser import parse_js
+from simon816.PHPParser import parse_php,p
 class HTMLHighlighter(HTMLParser):
  def do_highlighting(self,onstuff,html):
   self.do=onstuff
+  self.JS=False
   self.feed(html)
   self.close()
 
  def apply_tag(self,tagname,attributes=[],end=""):
+  if tagname=='script':
+   at=dict(attributes)
+   if 'type' in at:
+    if at['type']=='text/javascript':
+     self.JS=True
+   elif 'language' in at:
+     if at['language']=='javascript':
+       self.JS=True
+   elif len(attributes)==0:
+     self.JS=True
+  elif tagname=='/script':
+   self.JS=False
   self.do("tag","<"+tagname)
   raw=self.get_starttag_text()[1:-1]
   attrstr=raw[len(tagname)+1:]
@@ -45,7 +59,11 @@ class HTMLHighlighter(HTMLParser):
  def handle_startendtag(self,tag,attrs):
   self.apply_tag(tag,attrs," /")
  def handle_data(self,data):
-  self.do("text",data)
+  if self.JS:
+   for e in parse_js(data):
+    self.do(*e)
+  else:
+   self.do("text",data)
  def unknown_starttag(self,tag,attrs):
   self.handle_starttag(tag,None,attrs)
  def unknown_endtag(self,tag):
@@ -57,65 +75,72 @@ class HTMLHighlighter(HTMLParser):
   def unknown_entityref(self, entity):
    self.handle_entityref(entity)
 
-class JSHighlighter(JSParser):
- def __init__(self,do):
-  self.do=do
- def handle_data(self,data):
-  self.do('text',data)
- def handle_symbol(self,symbol):
-  self.do('symbol',symbol)
- def handle_statement(self,statement):
-  self.do('statement',statement)
- def handle_function(self,function):
-  self.do('function',function)
-
 class PHPHighlighter(HTMLHighlighter):
     def handle_php(self,php):
-     self.do("php","<?php"+php+"?>")
+      p=parse_php(php)
+      self.do("php","<?php")
+      for e in p:
+        self.do(*e)
+      self.do("php","?>")
 
 class SyntaxHighlighter(ExtendedEditor):
  def highlighter(self):
   self.extend()
+  self.keyword('\t',lambda:self.add(' '*self.tabsize))
+  # this may go in to ExtendedEditor base class
   self.languages={
    'HTML/XML':{
     'file_types':[".html",".htm",".xml"],
     'css':{
-     'tag':{'color':'#1100DF'},
-     'text':{'font-weight':'bold'},
-     'attr':{'color':'#EE1111'},
-     'attrval':{'color':'#4F4F4F'},
+     'tag':{'background': '#FFFFFF', 'color': '#0000FF'},
+     'text':{'background': '#FFFFFF', 'color': '#000000', 'font-weight': 'bold'},
+     'attr': {'background': '#FFFFFF', 'color': '#FF0000'},
+     'attrval':{'background': 'FEFDE0', 'color': 'FF8000'},
      'ent|char':{'font-style':'italic'},
      'pi':{'color':'#FF0000'},
-     'decl':{'color':'#111111','background':'#D0D0FF'},
-     'comment':{'color':'#207F20'}
+     'decl': {'background': '#A6CAF0', 'color': '#000000'},
+     'comment':{'background': '#FFFFFF', 'color': '#008000'}
     },
     'parser':HTMLHighlighter,
-    'function':self.open_html
+    'function':self.open_html,
+    'inherits':"Javascript"
    },
    'PHP':{
     'file_types':[".php"],
     'css': {
-     'php':{'color':'#FF0000'}
-    },
+     'php':{'color':'#FF0000'},
+     'keyword':{'font-weight': 'bold', 'background': '#FEFCF5', 'color': '#0000FF'},
+     'plain':{'background': 'FEFCF5', 'color': '000000'},
+     'variable':{'background': '#FEFCF5', 'color': '#000080'},
+     'string': {'background': '#FEFCF5', 'color': '#808080'},
+     'comment':{'background': '#FEFCF5', 'color': '#008000'},
+     'number': {'background': 'FEFCF5', 'color': 'FF8000'},
+     'invar': {'background': 'FEFCF5', 'color': '808080', 'font-weight': 'bold'},
+     'operator':{'background': '#FEFCF5', 'color': '#8000FF'}
+     },
     'inherits':"HTML/XML",
     'parser':PHPHighlighter,
     'function':self.open_php
    },
    'Javascript':{
-    'file_types':[".js.test"],
+    'file_types':[".js"],
     'css':{
-     'text':{},
+     'plaintext':{},
      'symbol':{'color':'#FF0000'},
      'statement':{'color':'#0000FF'},
      'function':{'color':'#00FF00'},
-     'keyword_css':{'color':'#2F11EF','font-style':'italic'}
+     'keyword_css':{'color':'#2F11EF','font-style':'italic'},
+     'string': {'color':'#808080'},
+     'comment':{'color':'#008000'},
+     'number': {'color':'#FF8000'},
+     'variable':{'color':'#000080'},
     },
-    'parser':JSHighlighter,
+    'parser':None,
     'function':self.open_javascript
    },
    'Text':{
     'file_types':["*"],
-    'css':{},
+    'css':{'Plain Text':self.on('get_text_css')},
     'parser':None,
     'function':self.open_text
    }
@@ -124,15 +149,25 @@ class SyntaxHighlighter(ExtendedEditor):
   self.menu.append((u'Language',
    tuple([(unicode(n),mksw(n)) for n in self.languages])
   ))
+ def inherit_css(self,lanset,c):
+   if 'inherits' in lanset:
+     l=self.languages[lanset['inherits']]
+     c.update(l['css'])
+     return self.inherit_css(l,c)
+   return c
  def swtch(self,t, lang,i=False):
+  self.clanguage=lang
   settings=self.languages[lang]
   self.ignore=i
+  """
   if 'inherits' in settings:
    settings['css']=dict(
     self.languages[
      settings['inherits']
      ]['css'].items()+settings['css'].items()
     )
+  """
+  settings['css']=self.inherit_css(settings,settings['css'])
   self.t.clear()
   settings['function'](t,settings['css'],settings['parser'])
   self.orig=self.getText()
@@ -151,14 +186,19 @@ class SyntaxHighlighter(ExtendedEditor):
  def open_html(self,html,css,p):
   apply=lambda k,v:self.apply_css(css,k,v)
   p().do_highlighting(apply,self.toUnicode(html))
+  apply('text','')
 
  def open_php(self,php,css,p):
   apply=lambda k,v:self.apply_css(css,k,v)
   p().do_highlighting(apply,self.toUnicode(php))
+  apply('text','')
 
  def open_javascript(self,js,css,p):
   apply=lambda k,v:self.apply_css(css,k,v)
-  p(apply).parse(js)
+  p=parse_js(js)
+  for a in p:
+    apply(*a)
+
 # overrides
  def loadText(self, t, ignore=False):
   ext='.'+self.name.split(".")[-1]
@@ -188,18 +228,39 @@ class SyntaxHighlighter(ExtendedEditor):
    css[k]=v
   self.cssSettings(css,
    lambda f:None)
-
-#"""
-import e32,StringIO
-lock=e32.Ao_lock()
-editor=SyntaxHighlighter()
-editor.highlighter()
-s=StringIO.StringIO()
-s.write("<html>text</html>")
-s.seek(0)
-editor.readFile(s,"test.html")
-editor.bind("exit",lock.signal)
-editor.inherit_indent()
-editor.display()
-lock.wait()
-#"""
+if __name__=='__main__':
+  import e32,StringIO
+  lock=e32.Ao_lock()
+  editor=SyntaxHighlighter()
+  editor.highlighter()
+  s=StringIO.StringIO()
+  s.write("<html>\n<?php\nfunction test($var='test') {\n//comment\nprint($var);\n/*\n  comment\n  block\n*/\necho \"hello \\\"w\\\\orl'd'\\\"\";\nprint \"\\$qwerty\";\nreturn new Test($var,9000);\n}\n?>\n<p style='display:none'>\ntext\n</p>\n<script>print \"hi\";</script></html>")
+  s.seek(0)
+  editor.readFile(s,"test.php")
+  editor.bind("exit",lock.signal)
+  editor.inherit_indent()
+  k=editor.key
+  def kp(code,inf):
+    k(code,inf)
+    if inf[0]=='backspace' or inf[0]=='enter':
+      return
+    def evaluate(pos):
+      l=editor.get_current_line()
+      editor.t.delete(p-len(l)+1,len(l))
+      s=editor.languages[editor.clanguage]
+      pp=parse_php(l)
+      for e in pp:
+        editor.apply_css(s['css'],*e)
+      #editor.add(l)
+    p=editor.t.get_pos()
+    e32.ao_sleep(0,lambda:evaluate(p))
+  #editor.key=kp
+  def kw(w):
+    editor.t.delete((editor.t.get_pos()-len(w))+1,len(w)-1)
+    s=editor.languages[editor.clanguage]
+    editor.apply_css(s['css'],'keyword',w[:-1])
+    editor.apply_css(s['css'],'text','')
+  for k in p.keywords:
+    editor.keyword(k,kw,k)
+  editor.display()
+  lock.wait()
