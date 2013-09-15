8bb6e04c7da500d9e2df76ea511e1fae881de92b
simon816 <simonbarnes1@googlemail.com>
2013-09-15 18:11:56 +0100

    Allows multiple key binding - must use bind_key method.
    Also improved test demo

--- CommonLib/simon816/ExtendedEditor.py
+++ CommonLib/simon816/ExtendedEditor.py
@@ -6,12 +6,17 @@ class ExtendedEditor(text):
   self.bind("key",self.keys)
   self.listen_keys={}
   del self.bindable[self.bindable.index("key")]
+ def bind_key(self, code, callback):
+   if type(code)==str:
+     code=ord(code)
+   if not code in self.listen_keys:
+     self.listen_keys[code]=[]
+   self.listen_keys[code].append(callback)
  def keys(self,code, properties):
-  self.last_key=k=properties[1]
-  if k in self.listen_keys:
-   self.listen_keys[k](code,properties)
-  elif code in self.listen_keys:
-   self.listen_keys[code](code,properties)
+  self.last_key=properties[1]
+  if code in self.listen_keys:
+   for fn in self.listen_keys[code]:
+     fn(code,properties)
  def change_color(self,r,g,b):
   self.t.color=self.rgb(r,g,b)
   self.t.add(u'')
@@ -22,7 +27,7 @@ class ExtendedEditor(text):
    if l>self.t.len():return
    before=self.t.get(self.t.get_pos()-l,l)
    if before==existing:callback(*args)
-  self.listen_keys[word[-1:]]=p
+  self.bind_key(word[-1:], p)
  def get_lines(self):
   return self.t.get()[:self.t.get_pos()].split(u'\u2029')
  def get_before_lines(self):
@@ -41,7 +46,7 @@ class ExtendedEditor(text):
   all_pre_lines=self.get_lines()
   return all_pre_lines[lineno]
  def get_pos_on_line(self):
-  pass
+  raise NotImplementedError('get_pos_on_line NYI')
  def inherit_indent(self):
   def on_enter(*a):
    current_line=self.get_current_line()
@@ -51,24 +56,23 @@ class ExtendedEditor(text):
     ends=current_line.count(end)
     if begins>ends:
      indent+=self.tabsize
-     break # ? could be a feature
+     break
     elif ends>begins:
      indent-=self.tabsize
-     break # ? could be a feature
+     break
    spaces=u''.join([' ' for i in range(indent)])
    addindent=lambda:self.t.add(spaces)
    ao_sleep(0,addindent) # an almost 0 delay between keypress and resulting key action
-  self.listen_keys[13]=on_enter
+  self.bind_key(13,on_enter)
  def add(self,text,i=False):
   self.t.add(self.toUnicode(text,i))
-"""
-e=ExtendedEditor()
-e.extend()
-import e32
-l=e32.Ao_lock()
-e.bind('exit',l.signal)
-e.loadText('<html>\n<head>\n</head>\n<body>\n</body>\n</html>')
-e.inherit_indent()
-e.display()
-l.wait()
-"""
+if __name__=='__main__':
+  e=ExtendedEditor()
+  e.extend()
+  import e32
+  l=e32.Ao_lock()
+  e.bind('exit',l.signal)
+  e.loadText('<html>\n<head>\n</head>\n<body>\n</body>\n</html>')
+  e.inherit_indent()
+  e.display()
+  l.wait()
