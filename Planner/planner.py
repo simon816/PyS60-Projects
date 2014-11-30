aee847f3aea2ae744e03f543236081cdb932861b
simon816 <simonbarnes1@googlemail.com>
2014-11-30 10:14:00 +0000

    Updates to planner

--- Planner/planner.py
+++ Planner/planner.py
@@ -1,13 +1,4 @@
-DEV_MODE = True
-
-import sys
-import re
-if DEV_MODE:
-    import dbcalendar as calendar
-else:
-    import calendar
-from simon816.interface import Interface
-from simon816.form import *
+from gui_lib import *
 from simon816.time_util import *
 
 lesson_len = Time(1, 5)
@@ -18,182 +9,248 @@ subjects = {
     'Maths': ((2, 4), (4, 5), (4, 6), (5, 4))
 }
 
-I = Interface()
-class Gui:
+DEV_MODE = True
+
+if DEV_MODE:
+    import dbcalendar as calendar
+else:
+    import calendar
+
+class Planner:
+
     def __init__(self):
-        a = ()
-        if DEV_MODE: a = ('e:\\devcal.db', 'c')
-        self.cal = calendar.open(*a)
-        self.closegui = I.current('exit')
-        self.main()
+        if DEV_MODE:
+            self.calendar = calendar.open('E:\\devcal.db', 'c')
+        else:
+            self.calendar = calendar.open()
+
+        self.make_screens()
+        self.show_main()
+
     def quit(self):
         if DEV_MODE:
-            self.cal.__del__()
-        self.closegui()
-    def main(self):
-        lb = I.listbox([
-            (u'Add Homework', self.add_h),
-            (u'Add Event', self.add_e),
-            (u'Upcomming', self.upcomming)
-        ])
-        I.set_screen(I.create_screen(body=lb,exit=self.quit))
+            self.calendar.__del__()
+        WM.exit()
 
-    def add_h(self):
-        ts =  DateTime.now().getAsTimestamp()
-        fSubj = ChoiceGroup('Subject', self.get_subjects())
-        fDesc = TextField('Description', '', TextField.ANY)
-        fDate = DateField('Due Date', DateField.DATE)
-        fDate.setDate(ts)
-        fTime = DateField('Due Time', DateField.TIME)
-        fTime.setDate(ts)
+    def make_screens(self):
+        self.main_screen = screen = Screen()
+        screen.body = lb = Listbox(ListboxType.SINGLE)
+        screen.title.set('Planner')
+        lb.add_item('Add Homework', self.add_homework)
+        lb.add_item('Add Event',  self.add_event)
+        lb.add_item('Upcomming', self.show_upcomming)
+        screen.exit.add_listener(self.quit)
 
-        f = Form('Add Homework', [fSubj, fDesc, fDate, fTime])
-        f.setEditable(True)
-        f.setDoubleSpace(True)
-        f.setMenu([(u'Find Time', lambda:self.find_time(f))])
-        f.setSaveHandler(self.save_h)
-        f.display()
+        self.new_homework_screen = screen = Form()
+        screen.title.set('Add Homework')
+        screen.add_item(ChoiceGroup('Subject', self.get_subjects()))
+        screen.add_item(TextField('Description', '', TextField.ANY))
+        screen.add_item(DateField('Due Date', DateField.DATE))
+        screen.add_item(DateField('Due Time', DateField.TIME))
+        screen.menu.add_item(MenuItem('Find Time', self.find_time))
+        screen.set_editable(True)
+        screen.set_double_spaced(True)
+        screen.exit.add_listener(self.save_homework)
+        screen.exit.add_listener(self.show_main)
 
-    def save_h(self, f):
-        fSubj = f.get(0)
-        fDesc = f.get(1)
-        fDate = f.get(2)
-        fTime = f.get(3)
+        self.new_event_screen = screen = Form()
+        screen.title.set('Add Event')
+        screen.add_item(ChoiceGroup('Type', ['General', 'Talk', 'Exam', 'Meeting']))
+        screen.add_item(TextField('What', '', TextField.ANY))
+        screen.add_item(TextField('Where', 'College', TextField.ANY))
+        screen.add_item(DateField('Day', DateField.DATE))
+        screen.add_item(DateField('When', DateField.TIME))
+        screen.set_editable(True)
+        screen.set_double_spaced(True)
+        screen.exit.add_listener(self.show_main)
+        screen.save.add_listener(self.save_event)
 
-        subj = fSubj.getString(fSubj.getSelectedIndex())             
-        e = self.cal.add_todo()
+        self.upcomming_screen = screen = Screen()
+        screen.title.set('Upcomming Events')
+        screen.body = Listbox(ListboxType.DOUBLE)
+        screen.menu.add_item(MenuItem('Mark as Done', self.mark_done))
+        screen.exit.add_listener(self.show_main)
 
-        e.content = '[HW] [' + subj + '] ' + fDesc.getString()
-        date = DateTime.fromTimestamp(fDate.getDate()).date
-        time = DateTime.fromTimestamp(fTime.getDate()).time
+        self.event_view_screen = screen = Form()
+        screen.title.set('View Entry')
+        screen.add_item(TextField('Subject', '', TextField.ANY))
+        screen.add_item(TextField('Description', '', TextField.ANY))
+        screen.add_item(TextField('Time', '', TextField.ANY))
+        screen.set_editable(True)
+        screen.set_double_spaced(True)
+        screen.menu.add_item(MenuItem('View Description', self.view_description))
+        #screen.exit.add_listener(self.show_upcomming)
+        screen.save.add_listener(lambda save_event: save_event.cancel())
+
+    def show_main(self):
+        WM.show(self.main_screen)
+
+    def add_homework(self):
+        now = DateTime.now().getAsTimestamp()
+        form = self.new_homework_screen
+        form.get_item(2).set_date(now)
+        form.get_item(3).set_date(now)
+        WM.show(form)
+
+    def save_homework(self, form_save_event=0):
+        screen = WM.get_current_screen()
+        form = screen#form_save_event.form
+
+        subject = form.get_item(0).get_selected_value()
+        description = form.get_item(1).get_value()
+        date = DateTime.fromTimestamp(form.get_item(2).get_value()).date
+        time = DateTime.fromTimestamp(form.get_item(3).get_value()).time
         datetime = DateTime(date, time)
-        print datetime
-        e.set_time(datetime.getAsTimestamp())
-        e.commit()
 
-    def add_e(self):
-        ts =  DateTime.now().getAsTimestamp()
-        fType = ChoiceGroup('Type', [u'General', u'Talk', u'Exam', u'Meeting'])
-        fWhat = TextField('What', '', TextField.ANY)
-        fWhere = TextField('Where', 'College', TextField.ANY)
-        fDay = DateField('Day', DateField.DATE)
-        fDay.setDate(ts)
-        fWhen = DateField('When', DateField.TIME)
-        fWhen.setDate(ts)
+        if description == '':
+            return
+        form.get_item(1).set_string('')
 
-        f = Form('Add Event', [fType, fWhat, fWhere, fDay, fWhen])
-        f.setEditable(True)
-        f.setDoubleSpace(True)
-        f.setSaveHandler(self.save_e)
-        f.display()
+        entry = self.calendar.add_todo()
 
-    def save_e(self, f):
-        fType = f.get(0)
-        fWhat = f.get(1)
-        fWhere = f.get(2)
-        fDay = f.get(3)
-        fWhen = f.get(4)
+        entry.content = '[HW] [%s] %s' % (subject, description)
+        entry.set_time(datetime.getAsTimestamp())
 
-        type = fType.getString(fType.getSelectedIndex())
+        entry.commit()
 
-        e = self.cal.add_event()
+    def add_event(self):
+        now = DateTime.now().getAsTimestamp()
+        form = self.new_event_screen
+        form.get_item(3).set_date(now)
+        form.get_item(4).set_date(now)
+        WM.show(form)
 
-        e.content = '[E] [' + type + '] ' + fWhat.getString()
-        e.location = fWhere.getString()
-        date = DateTime.fromTimestamp(fDay.getDate()).date
-        time = DateTime.fromTimestamp(fWhen.getDate()).time
+    def save_event(self, form_save_event):
+        form = form_save_event.form
+
+        type = form.get_item(0).get_selected_value()
+        what = form.get_item(1).get_value()
+        where = form.get_item(2).get_value()
+        date = DateTime.fromTimestamp(form.get_item(3).get_value()).date
+        time = DateTime.fromTimestamp(form.get_item(4).get_value()).time
         datetime = DateTime(date, time)
-        print datetime
-        e.set_time(datetime.getAsTimestamp())
-        e.commit()
+
+        entry = self.calendar.add_event()
+
+        entry.content = '[E] [%s] %s' % (type, what)
+        entry.location = where
+        entry.set_time(datetime.getAsTimestamp())
+
+        entry.commit()
+
+    def get_upcomming_entries(self):
+        from_time = DateTime.now()
+        from_time.addHours(-1)
+        from_time = DateTime(Date(2014), Time())
+        to_time = DateTime.now()
+        to_time.addDays(7)
+
+        instances = self.calendar.find_instances(
+            from_time.getAsTimestamp(),
+            to_time.getAsTimestamp(), '')
+
+        entries = []
+        for inst in instances:
+            entries.append(self.calendar[inst['id']])
+        return entries
+
+    def show_upcomming(self):
+        entries = self.get_upcomming_entries()
+        if len(entries) == 0:
+            Popup.alert('No recent entries')
+            return False
+        listbox = self.upcomming_screen.body
+
+        self.__entries = entries
+
+        listbox.clear()
+        import time
+        for entry in entries:
+            line1 = entry.content.replace('[HW] ', '')
+            line2 = time.strftime('%a %d %b, %H:%M', time.localtime(entry.end_time))
+            listbox.add_item(line1, line2)
+
+        listbox.default_callback(self.show_entry)
+
+        WM.show(self.upcomming_screen)
+        return True
 
     def get_subjects(self):
-        return map(unicode, subjects)
+        return subjects.keys()
 
-    def find_time(self, f):
-        fSubj = f.get(0)
-        subj = fSubj.getString(fSubj.getSelectedIndex())
-        times = self.get_subject(subj)
-        now = time.localtime()
-        l = []
-        s = []
-        for t in times:
-           days = 7-((now.tm_wday +8) - t[0] )%7
-           day = now.tm_mday + days
-           tm = self.get_p_time(t[1])
-           ts = time.localtime(time.mktime((now[0], now[1], day, tm[0], tm[1], tm[2], now[6], now[7], now[8])))
-           s.append(ts)
-           l.append(unicode(time.strftime('%a %d %b, %H:%M', (ts))))
-       
-        t = I.over_list(l)
-        if t is not None:
-            tm = time.mktime(s[t])
-            f.get(2).setDate(tm)
-            f.get(3).setDate(tm)
-           
+    def find_time(self):
+        form = WM.get_current_screen()
+        if not isinstance(form, Form):
+            Popup.error('Current body is not the form')
+            return
 
-    def get_subject(self, s):
-        return subjects[s]
+        subject = form.get_item(0).get_selected_value()
+        subj_periods = subjects[subject]
 
-    def get_p_time(self, p):
-        if p == 1:
-            return 9,0,0
-        if p==2:
-            return 10,5,0
-        if p == 3:
-            return 11,30,0
-        if p == 4:
-            return 12,35,0
-        if p == 5:
-           return 14,10,0
-        if p == 6:
-           return 15,15,0
+        times = []
+        times_popup_list = []
 
-    def upcomming(self):
-        all = self.cal.find_instances(time.time() - (60*60), time.time() + (60*60*24*7), '')
-        entrys = []
-        for i in all:
-            entrys.append(self.cal[i['id']])
-        if len(entrys) == 0:
-            I.alert('No recent entrys')
-            return False
-        items = []
-        def mk_cb(e): return lambda:self.open_e(e)
-        self._ent = entrys
-        for e in entrys:
-            e.timestr = unicode(time.strftime('%a %d %b, %H:%M', time.localtime(e.end_time)))
-            
-            items.append((e.content.replace('[HW] ', ''), e.timestr, mk_cb(e)))
-        lb=I.listbox(items)
-        I.set_screen(I.create_screen(body=lb, exit=self.main, menu=[
-            (u'Mark as Done', self.h_done)
-        ]))
+        import time as time_
+        st_time = time_.localtime()
 
-    def open_e(self, e):
-        m = re.match('\[(?:HW|St.*?ble)\]\ \[(\w+)\]\ (.*)', e.content)
-        if not m: return
-        f = Form('Entry')
-        f.append(TextField('Subject', m.group(1), TextField.ANY))
-        f.append(TextField('Description', m.group(2), TextField.ANY))
-        f.append(TextField('Time', e.timestr, TextField.ANY))
-        f.setDoubleSpace(True)
-        f.setEditable(True)
-        f.setMenu([(u'View Description', lambda:self.view_desc(f))])
-        f.display()
+        for period_tpl in subj_periods:
+            day, period = period_tpl
 
-    def h_done(self):
-        lb = I.current('body')
-        e = self._ent[lb.current()]
-        del self.cal[e.id]
-        if self.upcomming() == False:
-            self.main()
+            date = Date(st_time.tm_year, st_time.tm_mon, (7 - ((st_time.tm_wday + 8) - day) % 7) + st_time.tm_mday)
+            time = periods[period - 1]
 
-    def view_desc(self, f):
-        fDesc = f.get(1)
+            datetime = DateTime(date, time)
+            timestamp = datetime.getAsTimestamp()
+
+            times_popup_list.append(time_.strftime('%a %d %b, %H:%M',
+                                                   time_.localtime(timestamp)))
+            times.append(timestamp)
+
+        selected = Popup.menu(times_popup_list, 'Times for %s' % subject)
+        if selected is None:
+            return
+        form.get_item(2).set_date(times[selected])
+        form.get_item(3).set_date(times[selected])
+
+    def show_entry(self, index, value):
+        content, time = value
+        import re
+        #m = re.match('\[HW\]\ \[(\w+)\]\ (.*)', content)
+        m = re.match('\[(\w+)\]\ (.*)', content)
+        if not m:
+            Popup.error('Failed to match')
+            return
+        form = self.event_view_screen
+        form.get_item(0).set_string(m.group(1))
+        form.get_item(1).set_string(m.group(2))
+        form.get_item(2).set_string(time)
+        import thread
+        #thread.start_new_thread(WM.show, (form, ))
+        import e32
+        #e32.ao_sleep(0, lambda:WM.show(form))
+        WM.show(form)
+
+    def view_description(self):
+        form = WM.get_current_screen()
+        description = form.get_item(1).get_value()
+        #form.exit.remove_listener(self.show_upcomming)
         from simon816.text import text
         t = text()
-        t.loadText(fDesc.getString())
-        t.bind('exit', I.current('exit'))
+        t.loadText(description)
+        t.bind('exit', self.show_upcomming)
         t.display()
 
-Gui()
+    def mark_done(self):
+        listbox = WM.get_current_screen().body
+        entries = self.__entries
+        entry = entries[listbox.get_selected_index()]
+
+        del self.calendar[entry.id]
+
+        if self.show_upcomming() == False:
+            # No more entries
+            self.show_main()
+
+if __name__ == '__main__':
+    p = Planner()
+    WM.mainloop()
